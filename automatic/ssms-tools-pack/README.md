45b8224f23f4ffa938111ab3f3196a1e5d88a23e
AeroXuk <AeroXuk@users.noreply.github.com>
2020-01-29 20:48:29 +0000

    Adding SSMS Tools Pack v4.9.6.0

---
+++ automatic/ssms-tools-pack/README.md
@@ -0,0 +1,4 @@
+# [ssms-tools-pack](https://chocolatey.org/packages/ssms-tools-pack)
+
+
+SSMS Tools Pack is a Microsoft SQL Server Management Studio (SSMS) add-in that dramatically increases your productivity with well-integrated, easy to use features.
---
+++ automatic/ssms-tools-pack/update.ps1
@@ -0,0 +1,32 @@
+﻿[CmdletBinding()]
+param($IncludeStream, [switch]$Force)
+Import-Module AU
+
+$soapUri  = 'http://www.ssmstoolspack.com/ssmstoolspackservice.asmx?WSDL'
+$downloadUri = "https://www.ssmstoolspack.com/Download/STP?v="
+
+function global:au_BeforeUpdate {
+  $Latest.Checksum32 = Get-RemoteChecksum $Latest.URL32
+}
+
+function global:au_SearchReplace {
+  @{
+    ".\tools\chocolateyInstall.ps1" = @{
+      "(?i)(^\s*url\s*=\s*)('.*')"      = "`$1'$($Latest.URL32)'"
+      "(?i)(^\s*checksum\s*=\s*)('.*')" = "`$1'$($Latest.Checksum32)'"
+    }
+  }
+}
+
+function global:au_GetLatest {
+  $SsmsToolsPackService = New-WebServiceProxy -Uri $soapUri -Namespace SsmsToolsPackService
+  $latestVersion = $(Get-Version $($SsmsToolsPackService.ZadnjaVerzija())).ToString()
+
+  @{
+    Version = $latestVersion
+    RemoteVersion = $latestVersion
+    URL32   = "$downloadUri$($latestVersion)"
+  }
+}
+
+update -ChecksumFor none -IncludeStream $IncludeStream -Force:$Force
