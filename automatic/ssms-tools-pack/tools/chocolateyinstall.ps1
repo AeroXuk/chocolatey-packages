0ba60d7c9b41c12c8f92f67c702cb23cee1f422e
AeroXuk <AeroXuk@users.noreply.github.com>
2022-10-22 00:07:17 +0000

    AU: 1 updated - ssms-tools-pack [skip ci] https://gist.github.com/20a2279b8325cb6c751874e082b62e9b/0ec6db239a514e788d4ddb0cb8c7ec4f3f1e23f9

--- automatic/ssms-tools-pack/tools/chocolateyinstall.ps1
+++ automatic/ssms-tools-pack/tools/chocolateyinstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
   fileType      = 'msi'
-  url           = 'https://www.ssmstoolspack.com/Download/STP?v=5.9.1.0'
+  url           = 'https://www.ssmstoolspack.com/Download/STP?v=5.9.2.0'
 
   softwareName  = 'SSMS Tools Pack *'
 
-  checksum      = '6972ea6c08bad631b3cbdbf7fe9834fffd67b0e526779b8db9ccb1ea69166929'
+  checksum      = '4b787f10f939f392cf64ccafa06aa121d7313a16bd9af8e93c7a0f75aab94b5c'
   checksumType  = 'sha256'
 
   silentArgs    = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).$($env:chocolateyPackageVersion).MsiInstall.log`""
